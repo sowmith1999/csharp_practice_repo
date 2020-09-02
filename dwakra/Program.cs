c892ef19bb81aa64e958eb6e3e4668cf756c688f
Sowmith Kunapaneni <sowmithkunapaneni@gmail.com>
2020-09-03 00:03:04 +0530

    added another practice directory

---
+++ dwakra/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace dwakra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total=750000;
+            var monthly=25000;
+            var interest=0.75;
+            var noOfMonths=0;
+            do
+            {
+                total=total+interest*(total/100);
+                noOfMonths+=1;
+                total=total-25000;
+                System.Console.WriteLine(noOfMonths+" "+total);
+            } while (total>25000);
+            System.Console.WriteLine(noOfMonths);
+        }
+    }
+}
