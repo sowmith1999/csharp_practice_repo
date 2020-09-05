c9ca999f1e0a0354e07d9a55771e42f3729ab10d
Sowmith Kunapaneni <sowmithkunapaneni@gmail.com>
2020-09-05 12:36:52 +0530

    wrote down a binary search alogrithm, read a lot of djisktra, this commit teached me about gitignore and excludue files, helpful commit.

--- Algorithms_ToolBox_Week4/Algorithms_ToolBox_Week4/Program.cs
+++ Algorithms_ToolBox_Week4/Algorithms_ToolBox_Week4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 
@@ -12,9 +13,9 @@ namespace Algorithms_ToolBox_Week4
             var m = Console.ReadLine().Split(' ');
             var numberOfElements = long.Parse(k[0]);
             var inputArray = new long[numberOfElements];
-            for(var i=0;i<numberOfElements;i++)
+            for (var i = 0; i < numberOfElements; i++)
             {
-                inputArray[i]= long.Parse(k[i+1]);
+                inputArray[i] = long.Parse(k[i + 1]);
             }
             var elementsToSearch = long.Parse(m[0]);
             var searchArray = new long[elementsToSearch];
@@ -22,38 +23,74 @@ namespace Algorithms_ToolBox_Week4
             {
                 searchArray[i] = long.Parse(m[i + 1]);
             }
-            for(var i=1;i<=elementsToSearch;i++)
+            for (var i = 1; i <= elementsToSearch; i++)
             {
                 var hold = inputArray;
                 var tosearch = searchArray[i];
                 while (hold.Length > 2)
                 {
                     int middle = hold.Length / 2;
-                    if(hold[middle]==tosearch)
+                    if (hold[middle] == tosearch)
                     {
                         searchArray[i] = middle;
                     }
-                    else if(hold[middle]>tosearch)
+                    else if (hold[middle] > tosearch)
                     {
                         Array.Copy(hold, hold, middle);
                     }
-                    else if(hold[middle] > tosearch)
+                    else if (hold[middle] > tosearch)
                     {
-                        Array.Copy(hold,middle+1,hold,0,hold.Length-middle);
+                        Array.Copy(hold, middle + 1, hold, 0, hold.Length - middle);
                     }
                 }
             }
 
-           
-
         }
-        
+
     }
+
     class Program
     {
         static void Main(string[] args)
         {
-            BinarySearch.BinarySearchArray();
+            // BinarySearch.BinarySearchArray();
+            Console.WriteLine(BinarySearch());
+        }
+        public static Boolean BinarySearch()
+        {
+            var rawInputArray = Console.ReadLine().Split(' ');
+            var elementToSearch = int.Parse(Console.ReadLine());
+            var noOfElements = rawInputArray.Length;
+            var intInputList = new List<int> { };
+            for (var i = 0; i < rawInputArray.Length; i++)
+            {
+                intInputList.Add(int.Parse(rawInputArray[i]));
+            }
+            while (true)
+            {
+                var middle = intInputList[intInputList.Count / 2];
+                var middleIndex = intInputList.Count/2;
+                if (elementToSearch == middle)
+                {
+                    return true;
+                }
+                var hold = intInputList;
+                if(hold.Count==1)
+                {
+                    return false;
+                }
+                intInputList = new List<int> { };
+                if (elementToSearch > middle)
+                {
+                    intInputList = hold.GetRange(middleIndex,hold.Count-middleIndex);
+                }
+                else if (elementToSearch < middle)
+                {
+                    intInputList = hold.GetRange(0, middleIndex);
+                }
+            }
+
+
         }
     }
 }
--- CodeWars/CodeWars/HammingNumberGenerator.cs
+++ CodeWars/CodeWars/HammingNumberGenerator.cs
@@ -58,15 +58,15 @@ namespace CodeWars
     }
     class MainClass {
  
-        public static BigInteger Hamming(int n) {
-            BigInteger two = 2, three = 3, five = 5;
-            var h = new BigInteger[n];
+        public static long Hamming(int n) {
+            long two = 2, three = 3, five = 5;
+            var h = new long[n];
             h[0] = 1;
-            BigInteger x2 = 2, x3 = 3, x5 = 5;
+            long x2 = 2, x3 = 3, x5 = 5;
             int i = 0, j = 0, k = 0;
  
             for (int index = 1; index < n; index++) {
-                h[index] = BigInteger.Min(x2, BigInteger.Min(x3, x5));
+                h[index] = Math.Min(x2, Math.Min(x3, x5));
                 if (h[index] == x2) x2 = two * h[++i];
                 if (h[index] == x3) x3 = three * h[++j];
                 if (h[index] == x5) x5 = five * h[++k];
